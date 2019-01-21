bdc63fe31fe7c2c626c6d0d11353cd486fcb879e
theragingscotdan <43002238+theragingscotdan@users.noreply.github.com>
2019-01-21 13:09:18 +0000

    falling diamonds

--- Diamond.h
+++ Diamond.h
@@ -8,4 +8,12 @@ class Diamond : public GridObject
 
 public:
 	Diamond();
+
+private:
+	void Update(sf::Time _frameTime);
+	bool AttemptFall(sf::Vector2i _direction);
+
+	sf::Vector2i m_pendingFall;
+	float m_secondsPerFall;
+	bool m_IsFalling;
 };
--- Diamondcpp.cpp
+++ Diamondcpp.cpp
@@ -1,11 +1,72 @@
 // project includes
 #include "Diamond.h"
+#include "Level.h"
 #include "Framework/AssetManager.h"
 
 
 Diamond::Diamond()
 	: GridObject()
+	, m_pendingFall(0, 0)
+	, m_secondsPerFall(0)
+	, m_IsFalling(true)
 {
 	m_sprite.setTexture(AssetManager::GetTexture("graphics/box.png"));
 	m_blocksMovement = true;
+}
+
+void Diamond::Update(sf::Time _frameTime)
+{
+	m_secondsPerFall += _frameTime.asSeconds();
+	// If we have movement waiting to be processed,
+	if (m_secondsPerFall > 1.5f)
+	{
+		// move in that direction
+		bool moveSuccessful = AttemptFall(sf::Vector2i(0, 1));
+
+		m_secondsPerFall = 0;
+
+		/*
+
+
+					// and clear the pending movement
+					m_pendingFall = sf::Vector2i(0, 0);
+
+
+				} */
+	}
+}
+
+bool Diamond::AttemptFall(sf::Vector2i _direction)
+{
+	// Attempting to move in the given direction
+
+	// Get current position
+	//Calculate target position
+	sf::Vector2i targetPos = m_gridPosition + _direction;
+
+	// TODO: check if the space is empty
+	// Get list of objects in our target position
+	std::vector<GridObject*> targetCellContents = m_level->GetObjectAt(targetPos);
+
+	// Check if any of those objects block movement
+	bool blocked = false;
+	GridObject* blocker = nullptr;
+
+	for (int i = 0; i < targetCellContents.size(); ++i)
+	{
+		if (targetCellContents[i]->GetBlocksMovement() == true)
+		{
+			blocked = true;
+			blocker = targetCellContents[i];
+		}
+
+	}
+
+	// If empty, move there
+	if (blocked == false)
+		return m_level->MoveObjectTo(this, targetPos);
+
+	else
+
+		return false;
 }
